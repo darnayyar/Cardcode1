8436da50161c9c968e7b9d8b1c13048348a0ebd5
Admin <Admin@192.168.0.33>
2021-02-15 18:41:05 -0500

    added new object created in CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Darpan Nayyar 
+ * Student id: 991623744
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+     Card spades =new Card();  
     }
     
 }
