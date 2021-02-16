be8c4f452869aa729b9a14a5c60b33e9ba29ea58
Admin <Admin@192.168.0.33>
2021-02-15 19:58:55 -0500

    added new object created in CardTrick.java and created methods

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,7 +9,20 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-     Card spades =new Card();  
+     Card[] card =new Card[2];     
+     Card c1=new Card();
+     Card c2=new Card();
+     card[0]=c1;
+     card[1]=c2;
+     c1.setSuit("spades");
+      
+     c1.setValue(4);
+     c2.setSuit("Hearts");  
+     c2.setValue(7);
+     for (Card c: card){
+         
+     }
+     
     }
     
 }
