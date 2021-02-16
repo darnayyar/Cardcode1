8ad4325dee50c957dc10f8cd5acc577a53effca3
Admin <Admin@192.168.0.33>
2021-02-15 20:23:08 -0500

    final fix up of the code all methods and are already made

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -1,29 +1,26 @@
-
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Darpan Nayyar 
- * Student id: 991623744
+ * @author Darpan Nayyar Student id: 991623744
  */
 public class CardTrick {
-    public static void main(String[] args)
-    {
-     Card[] card =new Card[2];     
-     Card c1=new Card();
-     Card c2=new Card();
-     card[0]=c1;
-     card[1]=c2;
-     c1.setSuit("spades");
-      
-     c1.setValue(4);
-     c2.setSuit("Hearts");  
-     c2.setValue(7);
-     for (Card c: card){
+
+    public static void main(String[] args) {
+        Card[] card = new Card[2];
+        Card c1 = new Card();
+        Card c2 = new Card();
+        card[0] = c1;
+        card[1] = c2;
+        c1.setSuit("spades");
+        c1.setValue(4);
+        c2.setSuit("Hearts");
+        c2.setValue(7);
+        for (Card c : card) {
             System.out.println(c.getSuit());
-         System.out.println(c.getValue());
-     }
-     
+            System.out.println(c.getValue());
+        }
+
     }
-    
+
 }
